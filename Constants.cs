b43f1dbae7589863b0fd9482568d91dd0a7a2a9e
DeezVr <normajperez1030@gmail.com>
2026-05-17 12:11:09 -0400

    FIxed Source

---
+++ Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace Deez.TooMuchInfo;
+
+public static class Constants
+{
+    public const string PluginGuid    = "deez.toomuchinfo";
+    public const string PluginName    = "TooMuchInfo";
+    public const string PluginVersion = "1.0.3";
+}
---
+++ Plugin.cs
@@ -0,0 +1,101 @@
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using BepInEx;
+using HarmonyLib;
+using Newtonsoft.Json.Linq;
+using Photon.Pun;
+using Deez.TooMuchInfo.Tools;
+using Extensions = Deez.TooMuchInfo.Tools.Extensions;
+
+namespace Deez.TooMuchInfo;
+
+[BepInPlugin(Constants.PluginGuid, Constants.PluginName, Constants.PluginVersion)]
+public class Plugin : BaseUnityPlugin
+{
+    public static bool ShowSpecialCosmetics;
+    public static bool ShowKnownPeople;
+    public static bool ShowFpsPlatformPing;
+    public static bool ShowCreationDate;
+    public static bool ShowMods;
+
+    public static List<string> LineOrder;
+
+    public static Dictionary<string, string> KnownMods;
+    public static Dictionary<string, string> KnownCheats;
+    public static Dictionary<string, string> KnownPeople;
+    public static Dictionary<string, string> KnownCosmetics;
+
+    private void Awake()
+    {
+        ShowSpecialCosmetics = Config.Bind("Display", "SpecialCosmetics", true).Value;
+        ShowKnownPeople      = Config.Bind("Display", "KnownPeople",      true).Value;
+        ShowFpsPlatformPing  = Config.Bind("Display", "FpsPlatformPing",  true).Value;
+        ShowCreationDate     = Config.Bind("Display", "CreationDate",     true).Value;
+        ShowMods             = Config.Bind("Display", "Mods",             true).Value;
+
+        LineOrder = Config.Bind(
+                "Order",
+                "LineOrder",
+                "SpecialCosmetics,Name,FpsPlatformPing,CreationDate,Mods"
+        ).Value.Split(',').Select(x => x.Trim()).ToList();
+
+        new Harmony(Constants.PluginGuid).PatchAll();
+    }
+
+    private void Start() => GorillaTagger.OnPlayerSpawned(() =>
+                                                          {
+                                                              VRRigCache.OnRigDeactivated += rigContainer =>
+                                                                  {
+                                                                      VRRig vrrig = rigContainer.vrrig;
+                                                                      
+                                                                      Extensions.PlayersWithCosmetics.Remove(vrrig);
+                                                                      Extensions.PlayerPlatforms.Remove(vrrig);
+                                                                      Extensions.PlayerMods.Remove(vrrig);
+                                                                  };
+                                                              
+                                                              using HttpClient httpClient = new();
+                                                              HttpResponseMessage response =
+                                                                      httpClient.GetAsync(
+                                                                              "https://raw.githubusercontent.com/DeezVrOfficial/GorillaInfo/refs/heads/main/KnownPeople.txt").Result;
+
+                                                              using (Stream hamburburDataStream =
+                                                                     response.Content.ReadAsStreamAsync().Result)
+                                                              {
+                                                                  using (StreamReader reader = new(hamburburDataStream))
+                                                                  {
+                                                                      JObject root = JObject.Parse(reader.ReadToEnd());
+
+                                                                      KnownCheats =
+                                                                              root["knownCheats"]?
+                                                                                     .ToObject<Dictionary<string,
+                                                                                              string>>();
+
+                                                                      KnownMods =
+                                                                              root["knownMods"]?
+                                                                                     .ToObject<Dictionary<string,
+                                                                                              string>>();
+
+                                                                      KnownPeople =
+                                                                              root["knownPeople"]?
+                                                                                     .ToObject<Dictionary<string,
+                                                                                              string>>();
+
+                                                                      KnownCosmetics =
+                                                                              root["specialCosmetics"]?
+                                                                                     .ToObject<Dictionary<string,
+                                                                                              string>>();
+                                                                  }
+                                                              }
+                                                          });
+
+    private void Update()
+    {
+        if (VRRig.LocalRig.playerText1 == null || PhotonNetwork.LocalPlayer == null)
+            return;
+
+        VRRig.LocalRig.playerText1.text =
+                PhotonNetwork.LocalPlayer.NickName.AnimateGradient(VRRig.LocalRig.playerColor);
+    }
+}
