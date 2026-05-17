708b59e05948d7e15a3260d4ca1d4998d7e70ef7
DeezVr <normajperez1030@gmail.com>
2026-05-17 12:10:15 -0400

    Updated Source code!

--- Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Deez.TooMuchInfo;
-
-public static class Constants
-{
-    public const string PluginGuid = "deez.toomuchinfo";
-    public const string PluginName = "TooMuchInfo";
-    public const string PluginVersion = "1.0.2";
-    public const string Hashkey = "github.com/DeezVrOfficial/TooMuchInfo";
-}
