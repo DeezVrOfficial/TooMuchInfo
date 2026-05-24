5984a3c3cb510607f308f013cbd88e19b2b84597
DeezVr <normajperez1030@gmail.com>
2026-05-23 23:20:11 -0400

    Update Constants.cs

--- Constants.cs
+++ Constants.cs
@@ -2,9 +2,9 @@
 {
     internal class Constants
     {
-        public const string GUID = "zlothy.toomuchinfo";
+        public const string GUID = "deez.toomuchinfo";
         public const string Name = "TooMuchInfo";
-        public const string HashKey = "github.com/ZlothY29IQ/TooMuchInfo";
+        public const string HashKey = "github.com/DeezVrOffical/TooMuchInfo";
         public const string Version = "1.1.3";
     }
 }
